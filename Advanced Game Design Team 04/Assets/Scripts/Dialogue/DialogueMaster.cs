7358bcdac8d8fd3b10fff5bc79d47409833f37ce
Polik263 <a.dominik2001@interia.eu>
2022-11-17 17:41:03 +0100

    Dialogue Update

--- Advanced Game Design Team 04/Assets/Scripts/Dialogue/DialogueMaster.cs
+++ Advanced Game Design Team 04/Assets/Scripts/Dialogue/DialogueMaster.cs
@@ -10,8 +10,18 @@ public class DialogueMaster : MonoBehaviour
     private PlayerControls playerControls;
     private InputAction dialogue;
 
-    [SerializeField] private GameObject dialogueBox;
+    [SerializeField] private GameObject panel;
+
+
+    [SerializeField] private GameObject dialogueBox11;
+    [SerializeField] private GameObject dialogueBox12;
+    [SerializeField] private GameObject dialogueBox13;
+    [SerializeField] private GameObject dialogueBox21;
+    [SerializeField] private GameObject dialogueBox31;
     [SerializeField] private bool isInDialogue;
+
+    [SerializeField] private GameObject Choice11;
+    [SerializeField] private GameObject Choice12;
 
     void Awake()
     {
@@ -20,7 +30,14 @@ public class DialogueMaster : MonoBehaviour
 
     public void ActivateDialouge()
     {
-        dialogueBox.SetActive(true);
+        panel.SetActive(true);
+        dialogueBox11.SetActive(true);
+
+        if (dialogueBox11.activeInHierarchy == true)
+        {
+            Choice11.SetActive(true);
+            Choice12.SetActive(true);
+        }
     }
 
     void HandleDialouge(InputAction.CallbackContext context)
@@ -40,5 +57,30 @@ public class DialogueMaster : MonoBehaviour
         dialogue.Enable();
 
         dialogue.performed += HandleDialouge;
+    }
+
+
+    public void ChoiceOption11()
+    {
+        if (isInDialogue)
+        {
+            dialogueBox11.SetActive(false);
+            dialogueBox12.SetActive(true);
+
+            Choice11.SetActive(false);
+            Choice12.SetActive(false);
+        }
+    }
+
+    public void ChoiceOption12()
+    {
+        if (isInDialogue)
+        {
+            dialogueBox11.SetActive(false);
+            dialogueBox13.SetActive(true);
+
+            Choice11.SetActive(false);
+            Choice12.SetActive(false);
+        }
     }
 }
