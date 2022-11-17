f3b79d4dd8e508688a457dddd8dcac93d5ed6f16
Polik263 <a.dominik2001@interia.eu>
2022-11-17 21:05:10 +0100

    Dialogue Disappears after last line

--- Advanced Game Design Team 04/Assets/Scripts/Dialogue/DialogueMaster.cs
+++ Advanced Game Design Team 04/Assets/Scripts/Dialogue/DialogueMaster.cs
@@ -6,6 +6,10 @@ using UnityEngine.InputSystem;
 
 public class DialogueMaster : MonoBehaviour
 {
+    public GameObject[] lines;
+    public int linesNumber;
+    public int linesCount = 0;
+
 
     private PlayerControls playerControls;
     private InputAction dialogue;
@@ -23,13 +27,25 @@ public class DialogueMaster : MonoBehaviour
     [SerializeField] private GameObject dialogueBox14;
     [SerializeField] private GameObject dialogueBox15;
     [SerializeField] private GameObject dialogueBox21;
+    [SerializeField] private GameObject dialogueBox22;
+    [SerializeField] private GameObject dialogueBox23;
     [SerializeField] private GameObject dialogueBox31;
+    [SerializeField] private GameObject dialogueBox32;
+    [SerializeField] private GameObject dialogueBox33;
     [SerializeField] private bool isInDialogue;
 
     [SerializeField] private GameObject Choice11;
     [SerializeField] private GameObject Choice12;
     [SerializeField] private GameObject Choice13;
     [SerializeField] private GameObject Choice14;
+    [SerializeField] private GameObject Choice21;
+    [SerializeField] private GameObject Choice22;
+    [SerializeField] private GameObject Choice31;
+    [SerializeField] private GameObject Choice32;
+
+
+    public float offsetTime = 2f;
+    private float timer = 0f;
 
     void Awake()
     {
@@ -46,17 +62,61 @@ public class DialogueMaster : MonoBehaviour
     //    }
     //}
 
+    private void Update()
+    {
+        if (panel.activeInHierarchy == true)
+        {
+            isInDialogue= true;
+        }
+
+        if (dialogueBox33.activeInHierarchy == true || dialogueBox23.activeInHierarchy == true || dialogueBox13.activeInHierarchy == true)
+        {
+            Invoke("CloseDialogue", 5.0f);
+        }
+    }
+
+    void CloseDialogue()
+    {
+        panel.SetActive(false);
+        dialogueBox13.SetActive(false);
+        dialogueBox23.SetActive(false);
+        dialogueBox33.SetActive(false);
+        isInDialogue = false;
+    }
+        
+
     public void ActivateDialouge()
     {
         if (panel.activeInHierarchy == false)
         {
+            isInDialogue = false;
             panel.SetActive(true);
-            dialogueBox11.SetActive(true);
+
+            linesNumber = Random.Range(0, 3);
+            linesCount = 0;
+            while (linesCount < 1)
+            {
+                lines[linesCount].SetActive(false);
+                linesCount += 1;
+            }
+            lines[linesNumber].SetActive(true);
         }
         if (dialogueBox11.activeInHierarchy == true)
         {
             Choice11.SetActive(true);
             Choice12.SetActive(true);
+        }
+
+        if (dialogueBox21.activeInHierarchy == true)
+        {
+            Choice21.SetActive(true);
+            Choice22.SetActive(true);
+        }
+
+        if (dialogueBox31.activeInHierarchy == true)
+        {
+            Choice31.SetActive(true);
+            Choice32.SetActive(true);
         }
     }
 
@@ -128,6 +188,60 @@ public class DialogueMaster : MonoBehaviour
 
             Choice13.SetActive(false);
             Choice14.SetActive(false);
+        }
+    }
+
+    public void ChoiceOption21() 
+    {
+        if (isInDialogue && dialogueBox21.activeInHierarchy == true)
+        {
+            dialogueBox21.SetActive(false);
+            dialogueBox22.SetActive(true);
+
+            Choice21.SetActive(false);
+            Choice22.SetActive(false);
+
+            //Choice23.SetActive(true);
+            //Choice24.SetActive(true);
+        }
+    }
+
+    public void ChoiceOption22()
+    {
+        if (isInDialogue)
+        {
+            dialogueBox21.SetActive(false);
+            dialogueBox23.SetActive(true);
+
+            Choice21.SetActive(false);
+            Choice22.SetActive(false);
+        }
+    }
+
+    public void ChoiceOption31()
+    {
+        if (isInDialogue && dialogueBox21.activeInHierarchy == true)
+        {
+            dialogueBox31.SetActive(false);
+            dialogueBox32.SetActive(true);
+
+            Choice31.SetActive(false);
+            Choice32.SetActive(false);
+
+            //Choice23.SetActive(true);
+            //Choice24.SetActive(true);
+        }
+    }
+
+    public void ChoiceOption32()
+    {
+        if (isInDialogue)
+        {
+            dialogueBox31.SetActive(false);
+            dialogueBox33.SetActive(true);
+
+            Choice31.SetActive(false);
+            Choice32.SetActive(false);
         }
     }
 }
