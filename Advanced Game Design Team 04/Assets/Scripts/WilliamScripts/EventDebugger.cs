fbdd685e5c6a8964dd7cccb17bf2c568b809c107
Polik263 <a.dominik2001@interia.eu>
2023-03-22 14:24:51 +0100

    Revert "Revert "hard coded some dialogue events to be setup tomorrow""
    This reverts commit 695fe2390f7ccfda5fa49fe8eba0fff3fee94448.

---
+++ Advanced Game Design Team 04/Assets/Scripts/WilliamScripts/EventDebugger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventDebugger : MonoBehaviour
+{
+    public bool activated = false;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !activated)
+        {
+            DialogueManagerScript.Instance.Event5();
+            activated = true;
+        }
+    }
+}
